e0200c253cb36b464f369702e0eb30c69021b5b5
Jessica Tracy <jessicatracy@Jessicas-MacBook-Pro.local>
2016-08-18 15:00:47 -0400

    Added package and runner class

---
+++ src/com/tiy/practice/Assignment9Runner.java
@@ -0,0 +1,10 @@
+package com.tiy.practice;
+
+/**
+ * Created by jessicatracy on 8/18/16.
+ */
+public class Assignment9Runner {
+    public static void main(String[] args) {
+        System.out.println("Assignment9Runner running....");
+    }
+}
